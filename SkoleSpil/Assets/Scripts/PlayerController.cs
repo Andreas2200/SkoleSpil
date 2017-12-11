682f862efb39dff50129037e2566d91f49868811
Andreas2200 <Andreas.c.gade@gmail.com>
2017-12-11 15:34:19 +0100

    Testing
    Just messing around and trying to get things to work

---
+++ SkoleSpil/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject Player;
+    public GameObject MainCamera;
+
+    [SerializeField] private int moveSpeed = 5;
+   //[SerializeField] private int rotateSpeed = 5;
+
+    void Start ()
+    {
+        Cursor.visible = false;
+	}
+	
+
+	void Update ()
+    {
+        transform.Translate(Input.GetAxisRaw("Horizontal")*Time.deltaTime*moveSpeed, 0f,Input.GetAxisRaw("Vertical")*Time.deltaTime*moveSpeed);
+        //MainCamera.transform.Rotate(Input.GetAxisRaw("Mouse X") * Time.deltaTime * rotateSpeed, Input.GetAxisRaw("Mouse X") * Time.deltaTime * rotateSpeed, 0f);
+    }
+}
