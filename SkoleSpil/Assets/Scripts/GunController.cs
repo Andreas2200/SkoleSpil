79c5cacce8c7365261af090940ba22fc3a4b2264
Andreas2200 <Andreas.c.gade@gmail.com>
2017-12-12 19:40:31 +0100

    Finished version 1.0

---
+++ SkoleSpil/Assets/Scripts/GunController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public int bulletSpeed = 10;
+
+	void Start ()
+    {
+		
+	}
+	
+
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        var bullet = (GameObject) Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+        Destroy(bullet, 2f);
+    }
+}
--- SkoleSpil/Assets/Scripts/PlayerController.cs
+++ SkoleSpil/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,46 @@ public class PlayerController : MonoBehaviour {
     public GameObject MainCamera;
 
     [SerializeField] private int moveSpeed = 5;
-   //[SerializeField] private int rotateSpeed = 5;
+    [SerializeField] private int sensitivity = 5;
+    [SerializeField] private int gravity = 10;
+
+    private int jumpSpeed = 5;
+    private int rotateMin = -15;
+    private int rotateMax = 15;
+
+    private Vector3 moveDir = Vector3.zero;
+    private Vector3 v3Rotate = Vector3.zero;
 
     void Start ()
     {
+        MainCamera.transform.localEulerAngles = v3Rotate;
         Cursor.visible = false;
 	}
 	
 
 	void Update ()
     {
-        transform.Translate(Input.GetAxisRaw("Horizontal")*Time.deltaTime*moveSpeed, 0f,Input.GetAxisRaw("Vertical")*Time.deltaTime*moveSpeed);
-        //MainCamera.transform.Rotate(Input.GetAxisRaw("Mouse X") * Time.deltaTime * rotateSpeed, Input.GetAxisRaw("Mouse X") * Time.deltaTime * rotateSpeed, 0f);
+        CharacterController controller = GetComponent<CharacterController>();
+        if(controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= moveSpeed;
+            if(Input.GetButtonDown("Jump"))
+            {
+                moveDir.y = jumpSpeed;
+            }
+        }
+        moveDir.y -= gravity*Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+
+
+        v3Rotate.x += -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity;
+        v3Rotate.x = Mathf.Clamp(v3Rotate.x, rotateMin, rotateMax);
+        MainCamera.transform.localEulerAngles = v3Rotate;
+
+        //transform.Translate(Input.GetAxisRaw("Horizontal")*Time.deltaTime*moveSpeed, 0f,Input.GetAxisRaw("Vertical")*Time.deltaTime*moveSpeed);
+        transform.Rotate(0f, Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivity, 0f);
+        //MainCamera.transform.Rotate(-Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivity, 0f, 0f);
     }
 }
